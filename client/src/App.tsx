b28ce4ebd7cbad232c443621b925eaf17bbde3c7
MartinGassner <webdev@martin-gassner.com>
2020-03-21 12:26:58 +0100

    firestore example

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,124 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, MouseEvent } from 'react';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import uniqBy from 'lodash/uniqBy';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Item {
+  id: string,
+  title: string,
+  description: string
 }
 
-export default App;
+interface InputEvent {
+  target: {
+    name: string,
+    value: string,
+  }
+}
+
+export default class App extends Component {
+  state = {
+    data: [],
+    title: '',
+    description: ''
+  };
+
+  getItems = () => {
+    firebase
+      .firestore()
+      .collection('items')
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          this.setState({
+            data: uniqBy([...this.state.data, {...doc.data(), id: doc.id}], 'id')
+          });
+        });
+      });
+  };
+
+  componentDidMount() {
+    this.getItems();
+  }
+
+  dataIndex = () => {
+    return this.state.data.map((item: Item) => {
+      return (
+        <div key={item.id}>
+          <strong>Title: </strong>
+          {item.title} <strong>Description: </strong> {item.description}
+        </div>
+      );
+    });
+  };
+
+  handleSubmit = async (event: MouseEvent) => {
+    event.preventDefault();
+
+    // Add a new document with a generated id.
+    try {
+      await firebase
+        .firestore()
+        .collection('items')
+        .add({
+          title: this.state.title,
+          description: this.state.description
+        })
+
+
+      this.setState({
+        title: '',
+        description: ''
+      })
+
+      this.getItems()
+
+    } catch (e) {
+      console.error('Error adding document: ', e);
+    }
+  };
+
+  handleChange = (event: InputEvent) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  dataForm = () => {
+    return (
+      <form>
+        <input
+          type='text'
+          name='title'
+          placeholder='New Item'
+          value={this.state.title}
+          onChange={this.handleChange}
+        />
+        <input
+          type='text'
+          name='description'
+          placeholder='Item Description'
+          value={this.state.description}
+          onChange={this.handleChange}
+        />
+        <button type='submit' onClick={this.handleSubmit}>
+          Submit
+        </button>
+      </form>
+    );
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <h1> Form </h1>
+        {this.dataForm()}
+        <div>
+          <br />
+          {this.dataIndex()}
+        </div>
+      </div>
+    );
+  }
+}
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+}
+
+firebase.initializeApp(config);
+firebase.firestore();
 
 ReactDOM.render(
   <React.StrictMode>
