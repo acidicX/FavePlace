60fb3a725f46e420724f3374454d4751e3649897
Leonhard Melzer <leonhard.melzer@gmail.com>
2020-03-21 21:46:58 +0100

    fix: import prop

--- client/src/components/Map/Map.tsx
+++ client/src/components/Map/Map.tsx
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import { withRouter } from "react-router-dom";
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 import mapboxgl from 'mapbox-gl';
-import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder'
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import firebase from 'firebase/app';
 import data from '../../data.json';
 import 'firebase/firestore';
-import "./Map.css"
+import './Map.css';
 
 interface Location {
-  id: string,
-  title: string,
-  description: string,
+  id: string;
+  title: string;
+  description: string;
   coordinates: {
-    lat: number,
-    lng: number
-  }
+    lat: number;
+    lng: number;
+  };
 }
 
-
 interface State {
-  locations: Location[]
-  images: string[]
+  locations: Location[];
+  images: string[];
 }
 
 class Map extends Component<RouteComponentProps, State> {
@@ -29,61 +28,60 @@ class Map extends Component<RouteComponentProps, State> {
 
     this.state = {
       locations: [],
-      images: []
+      images: [],
     };
   }
 
-  map: any = null
+  map: any = null;
 
   componentDidMount() {
     if (process.env.REACT_APP_MAPBOX_ACCESS_TOKEN) {
-
-      mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
+      mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
       const map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v9'
-      })
+        style: 'mapbox://styles/mapbox/streets-v9',
+      });
 
       map.addControl(
         new MapboxGeocoder({
           accessToken: mapboxgl.accessToken,
           mapboxgl: mapboxgl,
-          marker: false
+          marker: false,
         })
       );
 
       map.addControl(
         new mapboxgl.GeolocateControl({
           positionOptions: {
-            enableHighAccuracy: true
+            enableHighAccuracy: true,
           },
-          trackUserLocation: true
+          trackUserLocation: true,
         })
       );
 
-      map.on('zoomend', this.fetchLocations)
+      map.on('zoomend', this.fetchLocations);
 
-      map.on('dragend', this.fetchLocations)
+      map.on('dragend', this.fetchLocations);
 
-      this.map = map
+      this.map = map;
 
       this.setState({
-        images: Object.keys(data.assets).map(key => key)
+        images: Object.keys(data.assets).map(key => key),
       });
     }
   }
 
   fetchLocations = async () => {
     if (this.map.isMoving() || this.map.getZoom() < 8) {
-      return
+      return;
     }
 
-    const { lng, lat } = this.map.getCenter()
+    const { lng, lat } = this.map.getCenter();
 
-    const { locations, images } = this.state
+    const { locations, images } = this.state;
 
-    let newLocations: Location[] = []
+    let newLocations: Location[] = [];
 
     await firebase
       .firestore()
@@ -91,15 +89,12 @@ class Map extends Component<RouteComponentProps, State> {
       .get()
       .then(querySnapshot => {
         querySnapshot.forEach(doc => {
-          const id = doc.id
+          const id = doc.id;
 
-          const isOnMap = locations.some(location => location.id === id)
+          const isOnMap = locations.some(location => location.id === id);
 
           if (!isOnMap) {
-            const {
-              title,
-              description
-            } = doc.data()
+            const { title, description } = doc.data();
 
             newLocations = [
               ...newLocations,
@@ -109,39 +104,37 @@ class Map extends Component<RouteComponentProps, State> {
                 id,
                 coordinates: {
                   lng: lng + (Math.random() - 0.5) / 10,
-                  lat: lat + (Math.random() - 0.5) / 10
+                  lat: lat + (Math.random() - 0.5) / 10,
                 },
-              }]
+              },
+            ];
           }
         });
       });
 
     this.setState({
-      locations: [
-        ...locations,
-        ...newLocations
-      ]
-    })
+      locations: [...locations, ...newLocations],
+    });
 
     for (const newLocation of newLocations) {
       const el = document.createElement('div');
       el.className = 'marker';
-      el.style.backgroundImage = `url("https://docs.mapbox.com/mapbox-gl-js/assets/washington-monument.jpg")`
+      el.style.backgroundImage = `url("https://docs.mapbox.com/mapbox-gl-js/assets/washington-monument.jpg")`;
 
       const marker = new mapboxgl.Marker(el)
         .setLngLat([newLocation.coordinates.lng, newLocation.coordinates.lat])
         .addTo(this.map);
 
       marker.getElement().addEventListener('click', () => {
-        this.props.history.push(`/view/${images[Math.floor(Math.random() * images.length)]}`)
+        this.props.history.push(`/view/${images[Math.floor(Math.random() * images.length)]}`);
       });
     }
-  }
+  };
 
   render() {
     return (
-      <div className='Map'>
-        <div id='map'></div>
+      <div className="Map">
+        <div id="map"></div>
       </div>
     );
   }
