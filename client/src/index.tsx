05efcff0832f6bf1c980418a59f86e9817807ad4
MartinGassner <webdev@martin-gassner.com>
2020-03-21 13:59:44 +0100

    client structure

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
