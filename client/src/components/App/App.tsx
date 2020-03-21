2e5c26c0f133ad8bc69ad58e5d46d6cd535ad9ec
acidicX <git@datahugger.de>
2020-03-21 22:39:04 +0100

    finish uploader

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Home } from '../Home/Home';
 import { View } from '../View/View';
+import UploadForm from '../UploadForm/UploadForm';
 
 export default () => (
   <div className="App">
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/view/:id" element={<View />} />
+      <Route path="/upload" element={<UploadForm />} />
     </Routes>
   </div>
 );
--- client/src/lib/uploadFile.ts
+++ client/src/lib/uploadFile.ts
@@ -1,23 +1,27 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from 'firebase';
 import { v4 } from 'uuid'
 import { MediaType } from '../types'
 
-export function uploadFile(type: MediaType, title: string, tags: Array<string>, blob: Blob): void {
+export async function uploadFile(type: MediaType, title: string, tags: Array<string>, blob: Blob) {
   const storageRef = firebase.storage().ref();
   const id = v4();
   const imageRef = storageRef.child(id);
-  imageRef.put(blob).then(function(snapshot) {
-    console.log('Uploaded a blob or file!');
-    firebase
-      .firestore()
-      .collection('items')
-      .add({
-        title,
-        tags,
-        type,
-        fullPath: snapshot.ref.fullPath
-      });
-  });
-
+  imageRef.put(blob)
+    .catch((e) => {
+      console.error(e);
+    })
+    .then(function(snapshot) {
+      console.log('Uploaded a blob or file!');
+      firebase
+        .firestore()
+        .collection('items')
+        .add({
+          title,
+          tags,
+          type,
+          fullPath: snapshot.ref.fullPath
+        });
+    }).catch((e) => {
+      console.error(e);
+    });
 }
