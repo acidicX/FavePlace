84859ef61148eb5e243eea2eaed906430d98437e
acidicX <git@datahugger.de>
2020-03-22 17:58:17 +0100

    fomat text center

--- client/src/components/About/About.tsx
+++ client/src/components/About/About.tsx
@@ -2,66 +2,72 @@ import React from 'react';
 import './About.css';
 
 export default function About() {
-    return (
-        <div className="about" >
-            <h1 className="heading">FavePlace</h1>
-            <div className="content">
-                <p>
-                    <span>Diese App ist im Rahmen des Hackathons </span>
-                    <a target="_blank" rel="noopener noreferrer" href="https://wirvsvirushackathon.org/">#wirwvsvirushack</a>
-                    <span> der Bundesregierung entstanden. Auf </span>
-                    <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href="https://devpost.com/software/favorite-place-taxi">
-                        DevPost
-                    </a>
-                    <span> gibt es detaillierte Informationen zum Projekt.</span>
-                </p>
-
-                <h3>Das Problem, das wir lösen wollen</h3>
-                <p>Aufgrund von Covid-19 und der erhöhten Ansteckungsgefahr wird es auf unabsehbare Zeit nicht mehr möglich sein, die eigenen Liebslingsorte zu besuchen, Ausflüge zu machen oder in den Urlaub zu fahren. Die fehlende Abwechslung führt bei vielen zu Frust und Unausgeglichenheit.</p>
-                <h3>Die Grundidee hinter der Lösung</h3>
-                <p>
-                    Jeder Mensch lebt an einem anderen Ort und hat je nach Ausmaß der Ausgangsbeschränkung einen unterschiedlichen Bewegungsradius. Das nutzen wir: So können wir 360° Panoramen von den unterschiedlichsten Orten in unserer community-basierten WebApp sammeln und wieder zur Verfügung stellen - ganz ohne die Ausgangsbeschränkungen zu verletzen und ohne Ansteckungen zu riskieren.
-                </p>
-                <h1>Impressum</h1>
-                <p>
-                    <span>FavePlace ist eine Open Source Software: </span>
-                    <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href="https://github.com/acidicX/FavePlace">
-                        https://github.com/acidicX/FavePlace
-                    </a>
-                </p>
-                <h3>Copyright / Urheberrecht und Nutzungsrecht</h3>
-                <p>
-                    Mit dem Hochladen der Bilder auf die Plattform gewähren sie dem Betreiber ein Nutzungsrecht für diese Bilder wie folgt:
-
-                    einfach
-                    zeitlich unbeschränkt
-                    räumlich unbeschränkt
-
-                    Die Übertragung und Einräumung weiterer Nutzungsrechte auf beziehungsweise für Dritte durch den Betreiber erfolgt nicht. Die  Weitergabe an Pressevertreter für eine redaktionelle Verwendung ist mit  Angabe des Urhebers (sofern verfügbar) zulässig.
-                    
-                </p>
-                <h3>Technischer Betrieb dieser Instanz</h3>
-
-                    unterstützt von:
-
-                    Peerigon GmbH
-
-                    Werner-von-Siemens-Str. 6
-
-                    86159 Augsburg
-
-                    Es gilt das Impressum der Peerigon GmbH.
-                    Es gilt die Datenschutzerklärung der Peerigon GmbH.
-
-
-            </div>
+  return (
+    <div className="about">
+      <div className="center">
+        <h1 className="heading">FavePlace</h1>
+        <div className="content">
+          <p>
+            <span>Diese App ist im Rahmen des Hackathons </span>
+            <a
+              target="_blank"
+              className="link"
+              rel="noopener noreferrer"
+              href="https://wirvsvirushackathon.org/"
+            >
+              #wirwvsvirushack
+            </a>
+            <span> der Bundesregierung entstanden. Auf </span>
+            <a
+              target="_blank"
+              className="link"
+              rel="noopener noreferrer"
+              href="https://devpost.com/software/favorite-place-taxi"
+            >
+              DevPost
+            </a>
+            <span> gibt es detaillierte Informationen zum Projekt.</span>
+          </p>
+          <h3>Das Problem, das wir lösen wollen</h3>
+          <p>
+            Aufgrund von Covid-19 und der erhöhten Ansteckungsgefahr wird es auf unabsehbare Zeit
+            nicht mehr möglich sein, die eigenen Liebslingsorte zu besuchen, Ausflüge zu machen oder
+            in den Urlaub zu fahren. Die fehlende Abwechslung führt bei vielen zu Frust und
+            Unausgeglichenheit.
+          </p>
+          <h3>Die Grundidee hinter der Lösung</h3>
+          <p>
+            Jeder Mensch lebt an einem anderen Ort und hat je nach Ausmaß der Ausgangsbeschränkung
+            einen unterschiedlichen Bewegungsradius. Das nutzen wir: So können wir 360° Panoramen
+            von den unterschiedlichsten Orten in unserer community-basierten WebApp sammeln und
+            wieder zur Verfügung stellen - ganz ohne die Ausgangsbeschränkungen zu verletzen und
+            ohne Ansteckungen zu riskieren.
+          </p>
+          <h1>Impressum</h1>
+          <p>
+            <span>FavePlace ist eine Open Source Software: </span>
+            <a
+              target="_blank"
+              className="link"
+              rel="noopener noreferrer"
+              href="https://github.com/acidicX/FavePlace"
+            >
+              https://github.com/acidicX/FavePlace
+            </a>
+          </p>
+          <h3>Copyright / Urheberrecht und Nutzungsrecht</h3>
+          <p>
+            Mit dem Hochladen der Bilder auf die Plattform gewähren sie dem Betreiber ein
+            Nutzungsrecht für diese Bilder wie folgt: einfach zeitlich unbeschränkt räumlich
+            unbeschränkt Die Übertragung und Einräumung weiterer Nutzungsrechte auf beziehungsweise
+            für Dritte durch den Betreiber erfolgt nicht. Die Weitergabe an Pressevertreter für eine
+            redaktionelle Verwendung ist mit Angabe des Urhebers (sofern verfügbar) zulässig.
+          </p>
+          <h3>Technischer Betrieb dieser Instanz</h3>
+          unterstützt von: Peerigon GmbH Werner-von-Siemens-Str. 6 86159 Augsburg Es gilt das
+          Impressum der Peerigon GmbH. Es gilt die Datenschutzerklärung der Peerigon GmbH.
         </div>
-
-    )
+      </div>
+    </div>
+  );
 }
