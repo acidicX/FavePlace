78dbd640e3942c221649bc974736d837f19f310d
acidicX <git@datahugger.de>
2020-04-13 15:50:57 +0200

    bump version

--- client/src/lib/uploadFile.ts
+++ client/src/lib/uploadFile.ts
@@ -1,6 +1,47 @@
 import firebase from 'firebase';
 import { v4 } from 'uuid';
+import Jimp from 'jimp';
 import { MediaType, FirebaseItem } from '../types';
+
+type ImageOptions = {
+  maxHeight: number;
+  maxWidth: number;
+  maxFileSize: number;
+}
+
+async function processImage(blob: Blob, options: ImageOptions): Promise<ArrayBuffer> {
+  let arrayBuffer;
+  const fileReader = new FileReader();
+  fileReader.onload = function(event) {
+    if (event.target) {
+      arrayBuffer = event.target.result;
+    }
+  };
+  fileReader.readAsArrayBuffer(blob);
+
+  return await Jimp.read(arrayBuffer)
+    .then(image => {
+      const { maxHeight, maxWidth, maxFileSize } = options;
+      const height = image.getHeight();
+      const width = image.getWidth();
+      const heightExceeded = height > maxHeight;
+      const widthExceeded = width > maxWidth;
+      const fileSizeExceeded = blob.size > maxFileSize;
+      if (heightExceeded && !widthExceeded) {
+        console.log("height exceeded")
+        image.resize(Jimp.AUTO, maxHeight);
+      }
+      if (widthExceeded) {
+        console.log("width exceeded")
+        image.resize(maxWidth, Jimp.AUTO);
+      }
+      if (!heightExceeded && !widthExceeded && fileSizeExceeded) {
+        console.log("file size exceeded")
+        image.quality(70);
+      }
+      return image.getBufferAsync(Jimp.MIME_JPEG)
+    })
+}
 
 export async function uploadFile(
   type: MediaType,
@@ -9,12 +50,44 @@ export async function uploadFile(
   geo: FirebaseItem['geo'],
   blob: Blob
 ) {
+  let processedFile: ArrayBuffer | Blob | void;
+  try {
+    if (type === "image") {
+      processedFile = await processImage(blob, {
+        maxHeight: 2160,
+        maxWidth: 3840,
+        maxFileSize: 10000000
+      });
+    }
+    if (type === "image360") {
+      processedFile = await processImage(blob, {
+        maxHeight: 4352,
+        maxWidth: 8704,
+        maxFileSize: 15000000
+      });
+    }
+    if (type === "video") {
+      if (blob.size < 50000000) {
+        processedFile = blob;
+      } else {
+        throw new Error('File size exceeded (> 50 MB)');
+      }
+    }
+    if (!processedFile) {
+      throw new Error('File could not be handled');
+    }
+  } catch (err) {
+    alert ('File could not be handled');
+    console.error(err);
+    return;
+  }
+
   const storageRef = firebase.storage().ref();
   const id = v4();
   const imageRef = storageRef.child(id);
 
   return imageRef
-    .put(blob)
+    .put(processedFile)
     .catch(e => {
       console.error(e);
     })
